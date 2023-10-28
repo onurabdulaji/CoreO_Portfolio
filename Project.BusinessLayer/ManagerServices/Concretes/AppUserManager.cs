be37a3c4380a131cdb0a2ca9947720f71b5c45bb
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-10-28 04:40:34 +0200

    # Kucuk Duzenlemeler

--- Project.BusinessLayer/ManagerServices/Concretes/AppUserManager.cs
+++ Project.BusinessLayer/ManagerServices/Concretes/AppUserManager.cs
@@ -1,5 +1,6 @@
 ﻿using Project.BusinessLayer.ManagerServices.Abstracts;
 using Project.DataAccessLayer.Repositories.Abstracts;
+using Project.DataAccessLayer.Repositories.Concretes;
 using Project.EntityLayer.Models;
 using System;
 using System.Collections.Generic;
@@ -9,19 +10,25 @@ using System.Threading.Tasks;
 
 namespace Project.BusinessLayer.ManagerServices.Concretes
 {
-    public class AppUserManager : BaseManager<AppUser>, IAppUserManager
-    {
-        IAppUserRepository _iappUserRepository; // Burada alir DAL'daki Interface'i Entity icin.
+	public class AppUserManager : BaseManager<AppUser>, IAppUserManager
+	{
+		IAppUserRepository _iappUserRepository; // Burada alir DAL'daki Interface'i Entity icin.
 
-        public AppUserManager(IAppUserRepository iappUserRepository) : base(iappUserRepository)
-        {
-            _iappUserRepository = iappUserRepository;
-        }
+		public AppUserManager(IAppUserRepository iappUserRepository) : base(iappUserRepository)
+		{
+			_iappUserRepository = iappUserRepository;
+		}
 
-        public async Task<bool> CreateUser(AppUser item, string Password)
-        {
-            // BL'daki CreateUser metodu icin UserRepository'in AddUser metodu ile User
-            return await _iappUserRepository.AddUser(item, Password);
-        }
-    }
+		public async Task<bool> CreateUser(AppUser item, string Password)
+		{
+			if (item.Email == null && item.UserName == null && Password == null) // Validation , farkli seyler de yapilabilir
+			{
+				return false;
+			}
+			return await _iappUserRepository.AddUser(item, Password);
+		}
+	}
 }
+
+//return await _iappUserRepository.AddUser(item, Password); // Burada Ekler
+
