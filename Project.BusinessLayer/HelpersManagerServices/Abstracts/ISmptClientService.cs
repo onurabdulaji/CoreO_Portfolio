ec85f862e86f0683c566bf35b6811f699d9b3dd1
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-10-29 20:28:49 +0100

    # Smpt Service # Smpt Manager
    Eklendi

---
+++ Project.BusinessLayer/HelpersManagerServices/Abstracts/ISmptClientService.cs
@@ -0,0 +1,14 @@
+﻿using MailKit.Net.Smtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BusinessLayer.HelpersManagerServices.Abstracts
+{
+    public interface ISmptClientService
+    {
+        SmtpClient CreateSmptClient(string smptpServer, int smptPort, string username, string password);
+    }
+}
---
+++ Project.BusinessLayer/HelpersManagerServices/Concretes/SmptClientManager.cs
@@ -0,0 +1,21 @@
+﻿using MailKit.Net.Smtp;
+using Project.BusinessLayer.HelpersManagerServices.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BusinessLayer.HelpersManagerServices.Concretes
+{
+    public class SmptClientManager : ISmptClientService
+    {
+        public SmtpClient CreateSmptClient(string smptpServer, int smptPort, string username, string password)
+        {
+            var client = new SmtpClient();
+            client.Connect(smptpServer, smptPort, false);
+            client.Authenticate(username, password);
+            return client;
+        }
+    }
+}
