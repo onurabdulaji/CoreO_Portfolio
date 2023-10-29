39f13269a0f0f9858e5a55a4b1e75e76ffcf0097
Onur Abdulaji <76725341+onurabdulaji@users.noreply.github.com>
2023-10-29 20:21:37 +0100

    # Helper Servisler Eklenecek

--- Project.BusinessLayer/ManagerServices/Concretes/AppUserManager.cs
+++ Project.BusinessLayer/ManagerServices/Concretes/AppUserManager.cs
@@ -10,24 +10,24 @@ using System.Threading.Tasks;
 
 namespace Project.BusinessLayer.ManagerServices.Concretes
 {
-	public class AppUserManager : BaseManager<AppUser>, IAppUserManager
-	{
-		IAppUserRepository _iappUserRepository; // Burada alir DAL'daki Interface'i Entity icin.
+    public class AppUserManager : BaseManager<AppUser>, IAppUserManager
+    {
+        IAppUserRepository _iappUserRepository; // Burada alir DAL'daki Interface'i Entity icin.
 
-		public AppUserManager(IAppUserRepository iappUserRepository) : base(iappUserRepository)
-		{
-			_iappUserRepository = iappUserRepository;
-		}
+        public AppUserManager(IAppUserRepository iappUserRepository) : base(iappUserRepository)
+        {
+            _iappUserRepository = iappUserRepository;
+        }
 
-		public async Task<bool> CreateUser(AppUser item, string Password)
-		{
-			if (item.Email == null && item.UserName == null && Password == null) // Validation , farkli seyler de yapilabilir
-			{
-				return false;
-			}
-			return await _iappUserRepository.AddUser(item, Password);
-		}
-	}
+        public async Task<bool> CreateUser(AppUser item, string Password)
+        {
+            if (item.Email == null && item.UserName == null && Password == null) // Validation , farkli seyler de yapilabilir
+            {
+                return false;
+            }
+            return await _iappUserRepository.AddUser(item, Password);
+        }
+    }
 }
 
 //return await _iappUserRepository.AddUser(item, Password); // Burada Ekler
--- Project.MVCUI/Areas/Admin/Controllers/AccountController.cs
+++ Project.MVCUI/Areas/Admin/Controllers/AccountController.cs
@@ -7,50 +7,51 @@ using Project.EntityLayer.Models;
 
 namespace Project.MVCUI.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	[AllowAnonymous]
-	public class AccountController : Controller
-	{
-		IAppUserManager _iappUserManager;
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        IAppUserManager _iappUserManager;
 
-		public AccountController(IAppUserManager iappUserManager)
-		{
-			_iappUserManager = iappUserManager;
-		}
+        public AccountController(IAppUserManager iappUserManager)
+        {
+            _iappUserManager = iappUserManager;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
 
-		[HttpGet]
-		public IActionResult SignUp()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> SignUp(SignUpVM signInVM)
-		{
-			var newUser = new AppUser
-			{
-				FirstName = signInVM.Name,
-				LastName = signInVM.Surname,
-				Email = signInVM.Mail,
-				UserName = signInVM.Username
-			};
-			bool result = await _iappUserManager.CreateUser(newUser, signInVM.Password);
-			if (result)
-			{
-				return RedirectToAction("SignIn", "Account");
-			}
-			else
-			{
-				ModelState.AddModelError(string.Empty, "Hey , Sorry but you have entered something wrong , Result Failed.");
-			}
-			return View(signInVM);
-		}
-	}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignUp(SignUpVM signInVM)
+        {
+            var newUser = new AppUser
+            {
+                FirstName = signInVM.Name,
+                LastName = signInVM.Surname,
+                Email = signInVM.Mail,
+                UserName = signInVM.Username
+            };
+            bool result = await _iappUserManager.CreateUser(newUser, signInVM.Password);
+            if (result)
+            {
+                return RedirectToAction("SignIn", "Account");
+            }
+            else
+            {
+                ViewBag.Erorr = "Hey , Sorry but you have entered something wrong , Result Failed.";
+                ModelState.AddModelError(string.Empty, ViewBag.Error);
+            }
+            return View(signInVM);
+        }
+    }
 }
